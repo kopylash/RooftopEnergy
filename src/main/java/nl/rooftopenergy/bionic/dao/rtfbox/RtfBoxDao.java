167e8031526df9383101f5eff164f0f91be03f0c
AIakovenko <aleks.iakovenko@gmail.com>
2014-11-23 12:53:06 +0200

    Added inheritance from Dao and added methods

--- src/main/java/nl/rooftopenergy/bionic/dao/rtfbox/RtfBoxDao.java
+++ src/main/java/nl/rooftopenergy/bionic/dao/rtfbox/RtfBoxDao.java
@@ -1,5 +1,22 @@
 package nl.rooftopenergy.bionic.dao.rtfbox;
 
 
-public interface RtfBoxDao {
+import nl.rooftopenergy.bionic.dao.Dao;
+import nl.rooftopenergy.bionic.entity.RtfBox;
+
+import java.util.List;
+
+public interface RtfBoxDao extends Dao<RtfBox, Integer> {
+
+    /**
+     * Returns list of boxes which belong to company.
+     * @returns list of boxes;
+     */
+    public List<RtfBox> findByCompanyId(Integer companyID);
+
+    /**
+     * Returns box which belongs to company.
+     * @returns  box;
+     */
+    public RtfBox findByRtfBoxId(Integer rtfBoxID);
 }
--- src/main/java/nl/rooftopenergy/bionic/dao/rtfboxdata/RtfBoxDataDao.java
+++ src/main/java/nl/rooftopenergy/bionic/dao/rtfboxdata/RtfBoxDataDao.java
@@ -1,5 +1,19 @@
 package nl.rooftopenergy.bionic.dao.rtfboxdata;
 
 
-public interface RtfBoxDataDao {
+import nl.rooftopenergy.bionic.dao.Dao;
+import nl.rooftopenergy.bionic.entity.RtfBox;
+import nl.rooftopenergy.bionic.entity.RtfBoxData;
+
+import java.util.List;
+
+public interface RtfBoxDataDao extends Dao<RtfBoxData, Integer> {
+
+    /**
+     * Returns list of boxes using id number;
+     * @param rtfBox comparing object
+     * @return list of boxes;
+     */
+    public List<RtfBoxData> findByRtfBoxId(RtfBox rtfBox);
+
 }
