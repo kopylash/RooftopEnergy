b8ec9841244ef8f006ed780e6b785621fa98665f
Timurdov <timurdov@ukr.net>
2014-12-01 12:29:12 +0200

    Add pages.js and weather

---
+++ src/main/webapp/resources/js/pages.js
@@ -0,0 +1,18 @@
+$(function(){
+    $("#loggedPage").css({ 'color': '#0062D2'});
+
+    $(".mainButtons").click(function() {
+
+        //window.location = this.id + ".html";
+
+        //$(".mainButtons").css({ 'color': '#000000'});
+        //$(this).css({ 'color': '#0062D2'});
+
+    });
+
+
+
+
+
+
+});
