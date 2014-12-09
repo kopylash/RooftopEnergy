72e5d0eb8139078b9f210dee79e4a3780498f9de
Bessus <andriy_bysykalo@ukr.net>
2014-12-10 00:09:07 +0200

    java.util.stream.Collectors deleted

--- src/main/java/nl/rooftopenergy/bionic/entity/Company.java
+++ src/main/java/nl/rooftopenergy/bionic/entity/Company.java
@@ -3,7 +3,7 @@ package nl.rooftopenergy.bionic.entity;
 import javax.persistence.*;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 /**
  * Created by UFO on 17.11.2014.
