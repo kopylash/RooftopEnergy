ee2464297f123e18b4753b49773ac7cbcc2d9539
Bessus <andriy_bysykalo@ukr.net>
2014-12-18 22:31:06 +0200

    JUnit dependency added User DTO for settings added UserSettingsDataResource REST for settings added JUnit Test for UserSettingsDataResource REST added

---
+++ src/main/java/nl/rooftopenergy/bionic/rest/UserSettingsDataResource.java
@@ -0,0 +1,72 @@
+package nl.rooftopenergy.bionic.rest;
+
+import nl.rooftopenergy.bionic.dao.company.CompanyDao;
+import nl.rooftopenergy.bionic.dao.user.UserDao;
+import nl.rooftopenergy.bionic.entity.Company;
+import nl.rooftopenergy.bionic.entity.User;
+import nl.rooftopenergy.bionic.rest.util.PrincipalInformation;
+import nl.rooftopenergy.bionic.transfer.UserDataTransfer;
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RestController;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Bess on 12/17/14.
+ */
+@RestController
+@Path("boxData")
+public class UserSettingsDataResource {
+    private final Logger log = Logger.getLogger(RtfBoxDataResource.class.getName());
+
+    @Inject
+    private CompanyDao companyDao;
+
+    @Inject
+    private UserDao userDao;
+
+    @Inject
+    private PrincipalInformation principalInformation;
+
+    /**
+     * @param description is the new description of current user account that is about to be saved.
+     */
+    @POST
+    @Path("/saveNewDescription")
+    public Response saveNewDescription(@FormParam("description") String description){
+        if (description == null) description = "";
+        Company company = principalInformation.getCompany();
+        company.setDescription(description);
+        companyDao.save(company);
+        return Response.ok().build();
+    }
+
+    /**
+     * @return settings information about current user
+     */
+    @GET
+    @Path("/getUserDescription")
+    @Produces(MediaType.APPLICATION_JSON)
+    public UserDataTransfer getUserDescription(){
+        String principalName = principalInformation.getPrincipalName();
+        User user = userDao.findByName(principalName);
+        Company company = user.getCompany();
+
+        String userName = user.getUsername();
+        String country = company.getCountry();
+        String province = company.getProvince();
+        String street = company.getStreet();
+        Integer zipCode = company.getZipcode();
+        String city = company.getTown();
+        String companyName = company.getCompanyName();
+        String panelType = company.getRtfBox().getPanelType();
+        String description = company.getDescription();
+
+        UserDataTransfer userDataTransfer = new UserDataTransfer(userName, description, panelType, companyName,
+                city, zipCode, street, province, country);
+
+        return userDataTransfer;
+    }
+}
