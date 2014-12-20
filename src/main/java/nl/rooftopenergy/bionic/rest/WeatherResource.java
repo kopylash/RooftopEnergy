bd336a5fad31b2217d535476dcaa54a9da813497
AIakovenko <aleks.iakovenko@gmail.com>
2014-12-20 19:48:23 +0200

    Added REST service to use weather information

--- src/main/java/nl/rooftopenergy/bionic/rest/WeatherResource.java
+++ src/main/java/nl/rooftopenergy/bionic/rest/WeatherResource.java
@@ -1,7 +1,168 @@
 package nl.rooftopenergy.bionic.rest;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import nl.rooftopenergy.bionic.pojo.weather.WeatherForecastForFiveDays;
+import nl.rooftopenergy.bionic.pojo.weather.WeatherForecastForSixteenDays;
+import nl.rooftopenergy.bionic.pojo.weather.info.Info;
+import nl.rooftopenergy.bionic.pojo.weather.info.Precipitation;
+import nl.rooftopenergy.bionic.pojo.weather.info.TemperatureInfo;
+import nl.rooftopenergy.bionic.transfer.WeatherDailyDataTransfer;
+import nl.rooftopenergy.bionic.transfer.WeatherFiveDaysDataTransfer;
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.io.*;
+import java.util.*;
+
 /**
- * Created by alex on 12/18/14.
+ * Created by Alex Iakovenko on 12/18/14.
  */
+@RestController
+@Path("weather")
 public class WeatherResource {
+    private static final Logger logger = Logger.getLogger(WeatherResource.class.getName());
+    private static final String DAILY_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=";
+    private static final String FIVE_DAYS_URL = "http://api.openweathermap.org/data/2.5/forecast?q=";
+    private static final int MAX_DAYS_FORECAST = 16;
+    private static final double ZERO_IN_KELVIN = 273.15;
+
+
+    @POST
+    @Path("daily")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<WeatherDailyDataTransfer> showDailyWeather(@FormParam("city") String city,
+                                                     @FormParam("days") String days){
+        String thisCity = city;
+        Integer thisDays = Integer.parseInt(days);
+        List<WeatherDailyDataTransfer> resultList = null;
+        if ( thisDays == null || thisDays <= 0 || thisDays > MAX_DAYS_FORECAST){
+            thisDays = MAX_DAYS_FORECAST;
+        }
+        try {
+            HttpResponse<JsonNode> response1 = Unirest.get( DAILY_URL + thisCity + "&cnt=" + thisDays + "&mode=json").asJson();
+            String json = response1.getBody().toString();
+            /*Writer out = new OutputStreamWriter(new FileOutputStream(new File("/home/alex/sixteen.json")));
+            out.write(json);
+            out.close();*/
+            ObjectMapper mapper = new ObjectMapper();
+            WeatherForecastForSixteenDays weather = mapper.readValue(json, WeatherForecastForSixteenDays.class);
+
+//            WeatherForecastForSixteenDays weather = mapper.readValue(new FileInputStream(new File("/home/alex/sixteen.json")), WeatherForecastForSixteenDays.class);
+
+            WeatherDailyDataTransfer dataTransfer;
+            List<TemperatureInfo> weatherInfoList = weather.getList();
+            resultList = new ArrayList<WeatherDailyDataTransfer>();
+            for (TemperatureInfo obj : weatherInfoList){
+               dataTransfer = getForecast(obj);
+                resultList.add(dataTransfer);
+
+            }
+        } catch (UnirestException e){
+            logger.warn(e.getMessage(), e);
+        } catch (JsonGenerationException e) {
+            logger.warn(e.getMessage(), e);
+        } catch (JsonMappingException e) {
+            logger.warn(e.getMessage(), e);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return resultList;
+    }
+
+    @POST
+    @Path("fiveDay")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<WeatherFiveDaysDataTransfer> showFiveDayWeather(@FormParam("city") String city){
+        List<WeatherFiveDaysDataTransfer> resultList = null;
+        String thisCity = city;
+        try {
+            HttpResponse<JsonNode> response1 = Unirest.get(FIVE_DAYS_URL + thisCity + "&mode=json").asJson();
+            String json = response1.getBody().toString();
+//            Writer out = new OutputStreamWriter(new FileOutputStream(new File("/home/alex/fiveDays.json")));
+//            out.write(json);
+//            out.close();
+            ObjectMapper mapper = new ObjectMapper();
+
+            WeatherForecastForFiveDays weather = mapper.readValue(json, WeatherForecastForFiveDays.class);
+
+//            WeatherForecastForFiveDays weather = mapper.readValue(new FileInputStream(new File("/home/alex/fiveDays.json")), WeatherForecastForFiveDays.class);
+
+            WeatherFiveDaysDataTransfer dataTransfer;
+            List<Info> weatherInfoList = weather.getList();
+            resultList = new ArrayList<WeatherFiveDaysDataTransfer>();
+            for (Info obj : weatherInfoList){
+                dataTransfer = getForecastFiveDays(obj);
+                resultList.add(dataTransfer);
+
+            }
+        } catch (UnirestException e){
+            logger.warn(e.getMessage(), e);
+        } catch (JsonGenerationException e) {
+            logger.warn(e.getMessage(), e);
+        } catch (JsonMappingException e) {
+            logger.warn(e.getMessage(), e);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return resultList;
+
+    }
+
+    private WeatherDailyDataTransfer getForecast(TemperatureInfo info){
+
+        WeatherDailyDataTransfer dataTransfer = new WeatherDailyDataTransfer();
+        dataTransfer.setDt(info.getDt());
+        dataTransfer.setTempDay(info.getTemp().getDay() - ZERO_IN_KELVIN);
+        dataTransfer.setTempNight(info.getTemp().getNight() - ZERO_IN_KELVIN);
+        dataTransfer.setPressure(info.getPressure());
+        dataTransfer.setHumidity(info.getHumidity());
+        dataTransfer.setSky(info.getWeather().get(0).getMain());
+        dataTransfer.setSkyDescription(info.getWeather().get(0).getDescription());
+        dataTransfer.setSkyIcon(info.getWeather().get(0).getIcon());
+        return dataTransfer;
+
+    }
+    private WeatherFiveDaysDataTransfer getForecastFiveDays(Info info){
+
+        WeatherFiveDaysDataTransfer dataTransfer = new WeatherFiveDaysDataTransfer();
+        dataTransfer.setDt(info.getDt());
+        dataTransfer.setDateText(info.getDt_txt());
+
+        Precipitation rain = info.getRain();
+        if (rain != null) {
+            dataTransfer.setRain(rain.get_3h());
+        } else {
+            dataTransfer.setRain(null);
+        }
+
+        Precipitation snow = info.getSnow();
+        if (snow != null) {
+            dataTransfer.setSnow(snow.get_3h());
+        } else {
+            dataTransfer.setSnow(null);
+        }
+
+        dataTransfer.setTempDay(info.getMain().getTemp_max() - ZERO_IN_KELVIN);
+        dataTransfer.setTempNight(info.getMain().getTemp_min() - ZERO_IN_KELVIN);
+        dataTransfer.setPressure(info.getMain().getPressure());
+        dataTransfer.setHumidity(info.getMain().getHumidity());
+        dataTransfer.setSky(info.getWeather().get(0).getMain());
+        dataTransfer.setSkyDescription(info.getWeather().get(0).getDescription());
+        dataTransfer.setSkyIcon(info.getWeather().get(0).getIcon());
+        dataTransfer.setClouds(info.getClouds().getAll());
+        return dataTransfer;
+
+    }
+
 }
--- src/tests/nl/rooftopenergy/bionic/rest/WeatherResourceTest.java
+++ src/tests/nl/rooftopenergy/bionic/rest/WeatherResourceTest.java
@@ -1,16 +1,50 @@
 package nl.rooftopenergy.bionic.rest;
 
+import nl.rooftopenergy.bionic.transfer.WeatherDailyDataTransfer;
+import nl.rooftopenergy.bionic.transfer.WeatherFiveDaysDataTransfer;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/applicationContextForUnitTests.xml"})
 public class WeatherResourceTest {
+
+    private static final String CITY = "Kyiv,UA";
+    @Inject
+    private WeatherResource weatherResource;
+
+    @BeforeClass
+    public static void setup() {
+        Authentication auth = new UsernamePasswordAuthenticationToken("rooftop", "energy");
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(auth);
+    }
 
     @org.junit.Test
     public void testShowDailyWeather() throws Exception {
+        int forecastsNumber = 16;
+
+        List<WeatherDailyDataTransfer> data = weatherResource.showDailyWeather(CITY, String.valueOf(forecastsNumber));
+        assertTrue(data.size() == forecastsNumber);
 
     }
 
     @org.junit.Test
     public void testShowFiveDayWeather() throws Exception {
+        List<WeatherFiveDaysDataTransfer> data = weatherResource.showFiveDayWeather(CITY);
+        assertNotNull(data);
 
     }
 }
