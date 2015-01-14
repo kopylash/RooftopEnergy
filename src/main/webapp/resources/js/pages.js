a5b2faa29d7e54d6c1d8f90faaec1e87cb75bafd
Timurdov <timurdov@ukr.net>
2015-01-14 22:06:46 +0200

    Added desctop rating page

--- src/main/webapp/resources/js/pages.js
+++ src/main/webapp/resources/js/pages.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function() {
     var firstHead = $('header').html();
 
     var companyName;
@@ -23,16 +23,14 @@ $(function(){
     }
 
 
+    ajaxGetUserInfo();
 
-
-       ajaxGetUserInfo();
-
-    $(document).ajaxComplete(function(){
+    $(document).ajaxComplete(function () {
         $("#desktopCompany").html(companyName);
         $("#desktopUser span").html(userName);
     });
     //function loadHtml() {
-        var code = '<div id="mainMenu"><div id="settingsMenu" class="ui-widget-content ui-corner-all">\
+    var code = '<div id="mainMenu"><div id="settingsMenu" class="ui-widget-content ui-corner-all">\
     <div id="settings" class="mainButtons hideButtons"><i class="fa fa-cogs"><span>&nbspSettings</span></i></div>\
     <div id="changePassword" class="mainButtons hideButtons"><i class="fa fa-key"><span>&nbspChange&nbsppassword</span></i></div>\
     <div id="logout" class="mainButtons hideButtons"><a href="j_spring_security_logout"><i class="fa fa-sign-out"><span>&nbspLogout</span></i></a></div></div>\
@@ -42,52 +40,55 @@ $(function(){
     <div id="rating" class="mainButtons"><i class="fa fa-long-arrow-down  fa-4x"></i><i class="fa fa-long-arrow-up  fa-4x"></i></div>\
     <div id="menu" class="mainButtons"><i class="fa fa-list  fa-4x"></i></div></div></div>';
 
-        var headerHtml = '<div class="desktopHeaderClass" id="desktopHeader">' +
-            '<div id="desktopLogo"><img src="../resources/images/logo.svg"></div> ' +
-            '<div id="desktopUser"><a href="j_spring_security_logout"><span></span> <i class="fa fa-sign-out"></i></a></div>' +
-            '<div id="desktopCompany"></div></div><div id="forClear"></div>';
+    var headerHtml = '<div class="desktopHeaderClass" id="desktopHeader">' +
+        '<div id="desktopLogo"><img src="../resources/images/logo.svg"></div> ' +
+        '<div id="desktopUser"><a href="j_spring_security_logout"><span></span> <i class="fa fa-sign-out"></i></a></div>' +
+        '<div id="desktopCompany"></div></div><div id="forClear"></div>';
 
-        function runEffect() {
-            $("#settingsMenu").show("slide", {direction: "right"}, 500);
-            $("#mainMenu").hover(function () {
-                $("#settingsMenu").hide("slide", {direction: "right"}, 500);
-            })
-        }
+    function runEffect() {
+        $("#settingsMenu").show("slide", {direction: "right"}, 500);
+        $("#mainMenu").hover(function () {
+            $("#settingsMenu").hide("slide", {direction: "right"}, 500);
+        })
+    }
 
 
-        //var foo = function(kod) {
-        //    if (screen.width <= 768) {
-        //        $('footer').html(kod).addClass("footer");
-        //    } else {
-        //        $('header').html(kod).removeClass("header").addClass("header1");
-        //        $("#consumption").html(" ");
-        //        var hh = $("#consumption").html();
-        //
-        //
-        //    }
-        //};
-        //foo(code);
+    //var foo = function(kod) {
+    //    if (screen.width <= 768) {
+    //        $('footer').html(kod).addClass("footer");
+    //    } else {
+    //        $('header').html(kod).removeClass("header").addClass("header1");
+    //        $("#consumption").html(" ");
+    //        var hh = $("#consumption").html();
+    //
+    //
+    //    }
+    //};
+    //foo(code);
 
-        var colorSwitch = function (k, kk) {
-            var page = window.location;
-            page = page + "";
-            var first = page.lastIndexOf("/") + 1;
-            var second = page.indexOf(".html");
-            var idi = page.slice(first, second);
-            $("#" + idi).css({'color': k, 'backgroundColor': kk});
+    var colorSwitch = function (k, kk) {
+        var page = window.location;
+        page = page + "";
+        var first = page.lastIndexOf("/") + 1;
+        var second = page.indexOf(".html");
+        var idi = page.slice(first, second);
+        $("#" + idi).css({'color': k, 'backgroundColor': kk});
 
-            $(".mainButtons").click(function () {
-                if (this.id == "menu") {
-                    runEffect();
-                } else {
-                    window.location = this.id + ".html";
-                }
-            });
+        $(".mainButtons").click(function () {
+            if (this.id == "menu") {
+                runEffect();
+            } else {
+                window.location = this.id + ".html";
+            }
+        });
 
-        };
+    };
 
-        colorSwitch();
+    colorSwitch();
 
+    window.addEventListener("resize", function () {
+        document.location.reload(true);
+    });
         $(window).resize(function () {
             if (screen.width <= 768) {
                 $('footer').html(code).addClass("footer");
@@ -109,7 +110,7 @@ $(function(){
                     $("#menu").text("settings").click(function () {
                         window.location = "settings.html";
                     });
-                    //$("#settings").addClass("bigButtonsFont");
+
                 });
 
                 $('footer').html(" ").removeClass('footer');
@@ -117,7 +118,6 @@ $(function(){
                 var k4 = "#108f38";
                 colorSwitch(k2, k4);
                 $("#desktopHeader").addClass("desktopHeaderClass");
-                //window.setTimeout('location.reload()', 60000);
 
             }
         });
