6f98aa849b078eb3385f660f1213ca69770ec4bd
Timurdov <timurdov@ukr.net>
2015-01-15 16:32:18 +0200

    There were added mobile libs

--- src/main/webapp/resources/js/loggedPage.js
+++ src/main/webapp/resources/js/loggedPage.js
@@ -4,6 +4,12 @@ $(function(){
     var productionPeriodUrl = '/rest/production';
     allDateButtons(productionPeriodUrl);
 
+    var isDevice = device.tablet();
+    var isAndroid =  device.androidTablet();
+    alert(isDevice + ' || ' + isAndroid);
+
+
+
 
 
 });
