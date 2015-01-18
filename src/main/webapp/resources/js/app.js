8e3182ad7627c1de96d5b93d4563fe6dac182191
Timurdov <timurdov@ukr.net>
2015-01-19 00:29:51 +0200

    Were added some angular directives

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,5 +1,5 @@
-$(function(){
-
+//$(function(){
+//
     $("#sub").click(function(){
         alert("Hello!!!");
     });
@@ -7,10 +7,33 @@ $(function(){
     var app = angular.module('passwordChange', []);
 
 
-    app.controller('passwordController', function(){
+    app.controller('passwordController', function($scope){
+        $scope.userOldPassword = "energy";
+        $scope.userOldEnterPassword = "";
+        $scope.userFirstNewPassword = "";
+        $scope.userSecondNewPassword = "";
 
+
+        $scope.showRightOld = false;
+        $scope.showFalseOld = false;
+        $scope.showRightNew = false;
+        $scope.showFalseNew = false;
+
+
+        $scope.newInputDisabled = true;
+        $scope.subButtonDisabled = true;
+
+
+        $scope.comperingPasswordFunction = function(){
+            if($scope.userOldEnterPassword == $scope.userOldPassword){
+                $scope.showRightOld = true;
+                $scope.newInputDisabled = false;
+            } else{
+                $scope.showFalseOld = false;
+            }
+        };
 
     });
 
 
-});
+//});
