8c8f4cb494bab0e613a31c10f24fd147665aa42f
AIakovenko <aleks.iakovenko@gmail.com>
2015-02-20 10:32:12 +0200

    Implemented rest resources to get ratio of production to consumption

--- src/main/java/nl/rooftopenergy/bionic/rest/MapDataResource.java
+++ src/main/java/nl/rooftopenergy/bionic/rest/MapDataResource.java
@@ -18,7 +18,13 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * Created by alex on 2/19/15.
+ * Provides REST API to get information about consumption and production of energy produced by
+ * solar panels to use data to show by regions.
+ * It is possible to get information about production and consumption which was produced or
+ * consumed by each region. Also it is able to get information about ratio between production
+ * and consumption.
+ *
+ * Created by Alexander Iakovenko on 2/19/15.
  */
 @RestController
 @Path("map")
@@ -40,85 +46,165 @@ public class MapDataResource {
     @Inject
     private MapData dutchProvinceList;
 
+    /**
+     * Gets list of regions and their production by current month.
+     * @return list of regions.
+     */
     @POST
     @Path("production/monthly")
     @Produces(MediaType.APPLICATION_JSON)
     public List<MapEntity> productionMonthly(){
 
         List<Company> companiesList = companyDao.findAll();
-        HashMap<String, Double> values= new HashMap<String, Double>();
+        HashMap<String, Double> companiesMap = new HashMap<String, Double>();
 
-        for (Company e : companiesList){
-            String key = e.getProvince();
-            Integer month = comparingDataResource.thisMonthTotalProduction(e.getCompanyName());
-            Double oldValue = dutchProvinceList.getEntity(key).getValue();
-            if (oldValue == null){
-                oldValue = 0d;
-            }
-            Double newValue;
-            if (values.containsKey(key)){
-                newValue = values.getOrDefault(key, 0d) + oldValue + month;
-                values.replace(key, newValue);
-            } else {
-                newValue = oldValue + month;
-                values.put(key, newValue);
-            }
+        for(Company c : companiesList){
+            String companyName = c.getCompanyName();
+            Integer value = comparingDataResource.thisMonthTotalProduction(companyName);
+            double castedValue = value == null ? 0d : (double)value;
+            companiesMap.put(companyName, castedValue);
         }
-        List<MapEntity> provinces = dutchProvinceList.getList();
-        for (MapEntity e : provinces){
-            e.setValue(values.get(e.getName()));
-        }
-        return provinces;
+
+        List<MapEntity> result = getList(companiesMap, companiesList);
+        return result;
     }
 
+    /**
+     * Gets list of regions and their production by current year.
+     * @return list of regions.
+     */
     @POST
     @Path("production/yearly")
     @Produces(MediaType.APPLICATION_JSON)
     public List<MapEntity> productionYearly(){
 
         List<Company> companiesList = companyDao.findAll();
-        HashMap<String, Double> values= new HashMap<String, Double>();
+        HashMap<String, Double> companiesMap = new HashMap<String, Double>();
 
-        for (Company e : companiesList){
-            String key = e.getProvince();
-            Integer year = comparingDataResource.thisYearTotalProduction(e.getCompanyName());
-            Double oldValue = dutchProvinceList.getEntity(key).getValue();
-            if (oldValue == null){
-                oldValue = 0d;
-            }
-            Double newValue;
-            if (values.containsKey(key)){
-                newValue = values.getOrDefault(key, 0d) + oldValue + year;
-                values.replace(key, newValue);
-            } else {
-                newValue = oldValue + year;
-                values.put(key, newValue);
-            }
+        for(Company c : companiesList){
+            String companyName = c.getCompanyName();
+            Integer value = comparingDataResource.thisYearTotalProduction(companyName);
+            double castedValue = value == null ? 0d : (double)value;
+            companiesMap.put(companyName, castedValue);
         }
-        List<MapEntity> provinces = dutchProvinceList.getList();
-        for (MapEntity e : provinces){
-            e.setValue(values.get(e.getName()));
-        }
-        return provinces;
+
+        List<MapEntity> result = getList(companiesMap, companiesList);
+        return result;
     }
 
+    /**
+     * Gets list of regions and their consumption by current month.
+     * @return list of regions.
+     */
     @POST
     @Path("consumption/monthly")
     @Produces(MediaType.APPLICATION_JSON)
     public List<MapEntity> consumptionMonthly(){
 
         List<Company> companiesList = companyDao.findAll();
-        HashMap<String, Double> values= new HashMap<String, Double>();
+        HashMap<String, Double> companiesMap = new HashMap<String, Double>();
 
-        for (Company e : companiesList){
+        for(Company c : companiesList){
+            String companyName = c.getCompanyName();
+            Integer value = comparingDataResource.thisMonthTotalConsumption(companyName);
+            double castedValue = value == null ? 0d : (double)value;
+            companiesMap.put(companyName, castedValue);
+        }
+
+        List<MapEntity> result = getList(companiesMap, companiesList);
+        return result;
+    }
+
+    /**
+     * Gets list of regions and their consumption by current year.
+     * @return list of regions.
+     */
+    @POST
+    @Path("consumption/yearly")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<MapEntity> consumptionYearly(){
+
+        List<Company> companiesList = companyDao.findAll();
+        HashMap<String, Double> companiesMap = new HashMap<String, Double>();
+
+        for(Company c : companiesList){
+            String companyName = c.getCompanyName();
+            Integer value = comparingDataResource.thisYearTotalProduction(companyName);
+            double castedValue = value == null ? 0d : (double)value;
+            companiesMap.put(companyName, castedValue);
+        }
+
+        List<MapEntity> result = getList(companiesMap, companiesList);
+        return result;
+    }
+
+    /**
+     * Gets list of regions and their ratio between production and consumption
+     * by current month.
+     * @return list of regions.
+     */
+    @POST
+    @Path("ratio/monthly")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<MapEntity> ratioMonthly(){
+
+        List<Company> companiesList = companyDao.findAll();
+        HashMap<String, Double> companiesMap = new HashMap<String, Double>();
+
+        for(Company c : companiesList){
+            String companyName = c.getCompanyName();
+            Integer production = comparingDataResource.thisMonthTotalProduction(companyName);
+            Integer consumption = comparingDataResource.thisMonthTotalConsumption(companyName);
+            double castedProduction = (production == null) || (production == 0) ? 1.0 : (double)production;
+            double castedConsumption = (consumption == null) || (consumption == 0) ? 1.0 : (double)consumption;
+            double ratio = castedProduction/castedConsumption;
+            companiesMap.put(companyName, ratio);
+        }
+
+        List<MapEntity> result = getList(companiesMap, companiesList);
+        return result;
+    }
+
+    /**
+     * Gets list of regions and their ratio between production and consumption
+     * by current month.
+     * @return list of regions.
+     */
+    @POST
+    @Path("ratio/monthly")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<MapEntity> ratioYearly(){
+
+        List<Company> companiesList = companyDao.findAll();
+        HashMap<String, Double> companiesMap = new HashMap<String, Double>();
+
+        for(Company c : companiesList){
+            String companyName = c.getCompanyName();
+            Integer production = comparingDataResource.thisYearTotalProduction(companyName);
+            Integer consumption = comparingDataResource.thisYearTotalConsumption(companyName);
+            double castedProduction = (production == null) || (production == 0) ? 1.0 : (double)production;
+            double castedConsumption = (consumption == null) || (consumption == 0) ? 1.0 : (double)consumption;
+            double ratio = castedProduction/castedConsumption;
+            companiesMap.put(companyName, ratio);
+        }
+
+        List<MapEntity> result = getList(companiesMap, companiesList);
+        return result;
+    }
+
+    private List<MapEntity> getList(HashMap<String, Double> companiesData, List<Company> companiesList) {
+
+        HashMap<String, Double> values = new HashMap<String, Double>();
+        for (Company e : companiesList) {
             String key = e.getProvince();
-            Integer month = comparingDataResource.thisMonthTotalConsumption(e.getCompanyName());
+            String companyName = e.getCompanyName();
+            Double month = companiesData.get(companyName);
             Double oldValue = dutchProvinceList.getEntity(key).getValue();
+            Double newValue;
             if (oldValue == null){
                 oldValue = 0d;
             }
-            Double newValue;
-            if (values.containsKey(key)){
+            if (values.containsKey(key)) {
                 newValue = values.getOrDefault(key, 0d) + oldValue + month;
                 values.replace(key, newValue);
             } else {
@@ -127,38 +213,7 @@ public class MapDataResource {
             }
         }
         List<MapEntity> provinces = dutchProvinceList.getList();
-        for (MapEntity e : provinces){
-            e.setValue(values.get(e.getName()));
-        }
-        return provinces;
-    }
-
-    @POST
-    @Path("consumption/yearly")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<MapEntity> consumptionYearly(){
-
-        List<Company> companiesList = companyDao.findAll();
-        HashMap<String, Double> values= new HashMap<String, Double>();
-
-        for (Company e : companiesList){
-            String key = e.getProvince();
-            Integer year = comparingDataResource.thisYearTotalConsumption(e.getCompanyName());
-            Double oldValue = dutchProvinceList.getEntity(key).getValue();
-            if (oldValue == null){
-                oldValue = 0d;
-            }
-            Double newValue;
-            if (values.containsKey(key)){
-                newValue = values.getOrDefault(key, 0d) + oldValue + year;
-                values.replace(key, newValue);
-            } else {
-                newValue = oldValue + year;
-                values.put(key, newValue);
-            }
-        }
-        List<MapEntity> provinces = dutchProvinceList.getList();
-        for (MapEntity e : provinces){
+        for (MapEntity e : provinces) {
             e.setValue(values.get(e.getName()));
         }
         return provinces;
--- src/tests/nl/rooftopenergy/bionic/rest/MapDataResourceTest.java
+++ src/tests/nl/rooftopenergy/bionic/rest/MapDataResourceTest.java
@@ -1,13 +1,104 @@
 package nl.rooftopenergy.bionic.rest;
 
+import nl.rooftopenergy.bionic.rest.util.MapEntity;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+
+import java.util.HashMap;
+import java.util.List;
 
 import static org.junit.Assert.*;
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/applicationContextForUnitTests.xml"})
 public class MapDataResourceTest {
+
+    @Inject
+    private MapDataResource mapDataResource;
+
+    @BeforeClass
+    public static void setup() {
+        Authentication auth = new UsernamePasswordAuthenticationToken("target", "qwerty");
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(auth);
+    }
 
     @Test
     public void testProductionMonthly() throws Exception {
+        List<MapEntity> list = mapDataResource.productionMonthly();
+        double result = 0d;
+        for (MapEntity e : list){
+            Double val = e.getValue();
+            result += val != null ? val : 0d ;
+        }
+        assertTrue(result > 0);
+
+    }
+
+    @Test
+    public void testProductionYearly() throws Exception {
+        List<MapEntity> list = mapDataResource.productionYearly();
+        double result = 0d;
+        for (MapEntity e : list){
+            Double val = e.getValue();
+            result += val != null ? val : 0d ;
+        }
+        assertTrue(result > 0);
+
+    }
+
+    @Test
+    public void testConsumptionMonthly() throws Exception {
+        List<MapEntity> list = mapDataResource.consumptionMonthly();
+        double result = 0d;
+        for (MapEntity e : list){
+            Double val = e.getValue();
+            result += val != null ? val : 0d ;
+        }
+        assertTrue(result > 0);
+    }
+
+    @Test
+    public void testConsumptionYearly() throws Exception {
+        List<MapEntity> list = mapDataResource.consumptionYearly();
+        double result = 0d;
+        for (MapEntity e : list){
+            Double val = e.getValue();
+            result += val != null ? val : 0d ;
+        }
+        assertTrue(result > 0);
+
+    }
+
+    @Test
+    public void testRatioMonthly() throws Exception {
+        List<MapEntity> list = mapDataResource.ratioMonthly();
+        double result = 0d;
+        for (MapEntity e : list){
+            Double val = e.getValue();
+            result += val != null ? val : 0d ;
+        }
+        assertTrue(result > 0);
+
+    }
+
+    @Test
+    public void testRatioYearly() throws Exception {
+        List<MapEntity> list = mapDataResource.ratioYearly();
+        double result = 0d;
+        for (MapEntity e : list){
+            Double val = e.getValue();
+            result += val != null ? val : 0d ;
+        }
+        assertTrue(result > 0);
 
     }
 }
